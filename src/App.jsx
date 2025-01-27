fabbad51d11fa66976bae3a76e2a0f940976af1c
Neha Rajpoot <neharajpoot233@gmail.com>
2025-01-27 11:09:49 +0530

    routes of each page is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import { useState } from 'react'
+import { Route,Routes } from 'react-router-dom'
+
+
+
+import About from "./pages/about.jsx"
+import Contact from "./pages/contact.jsx"
+import Home from "./pages/home.jsx"
+import Project from "./pages/project.jsx"
+import Service from "./pages/service.jsx"
 
 
 
@@ -8,7 +16,17 @@ function App() {
 
   return (
     <>
-    <div>basic setup is done</div>
+    <div>
+      <Routes>
+        
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/project" element={<Project/>} />
+        <Route path="/service" element={<Service/>} />
+      </Routes>
+    </div>
+
     </>
   )
 }
