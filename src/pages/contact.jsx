818536d6ba582129aa0e61f1132e249fc8810c5e
Neha Rajpoot <neharajpoot233@gmail.com>
2025-02-01 19:18:31 +0530

    contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { Mail, Phone, MapPin } from "lucide-react";
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 
 function Contact() {
+    const contacts = [
+        { icon: <Phone size={28} />, title: "Phone", detail: "+91 98765 43210" },
+        { icon: <Mail size={28} />, title: "Email", detail: "contact@example.com" },
+        { icon: <MapPin size={28} />, title: "Location", detail: "Banda, Uttar Pradesh" },
+      ];
     return (
-        <div>contact</div>
+        <>
+        <div>
+            <Navbar/>
+        </div>
+        <div>
+            <div className='space-y-4'>
+                <h1 className='flex justify-center text-center mt-44 font-serif font-semibold text-xl '>Contact us</h1>
+                <h1 className='flex justify-center text-center md:text-5xl font-serif font-bold text-gray-800'>Get in Touch, Reach Out for </h1>
+                <h1 className='flex justify-center text-center md:text-5xl font-serif font-bold text-gray-800' >Home Repair Solutions and More!</h1>
+            </div>
+            <div>
+            <div className="flex justify-start ml-32 min-h-screen items-center   p-32">
+      <div className="grid gap-12">
+        {contacts.map((contact, index) => (
+          <div
+            key={index}
+            className="flex items-center bg-gray-200 p-8  shadow-3xl rounded-3xl w-96 hover:scale-105 transition-transform duration-300 transform hover:shadow-xl border-l-8 border-blue-600"
+          >
+            <div className="p-5 bg-blue-600 text-white rounded-full shadow-lg">{contact.icon}</div>
+            <div className="ml-8">
+              <h3 className="text-2xl font-bold text-gray-900">{contact.title}</h3>
+              <p className="text-gray-600 font-medium text-lg">{contact.detail}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+            </div>
+
+        </div>
+        <div className='mt-96'>
+            <Footer/>
+        </div>
+        </>
     )
 }
 
--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 
 function Project() {
     return (
-        <div>project</div>
+        <>
+        <div>
+            <Navbar/>
+        </div>
+        <div className='mt-32'>
+            <h1 className='md:ml-32'>OUR WORK</h1>
+            <h1 className='md:ml-32 text-5xl font-bold md:mr-96'>Building the future of the construction industry, one project at a time</h1>
+        </div>
+        <div className='mt-44'>
+            <Footer/>
+        </div>
+        </>
     )
 }
 
